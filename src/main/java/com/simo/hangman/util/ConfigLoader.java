c49eb5e59d872cc550b28e28990dee386f850903
lucas.simonini <lucas.simonini@backcountry.com>
2021-07-11 12:00:05 -0300

    configuration loading

--- src/main/java/com/simo/hangman/util/ConfigLoader.java
+++ src/main/java/com/simo/hangman/util/ConfigLoader.java
@@ -2,19 +2,25 @@ package com.simo.hangman.util;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.simo.hangman.domain.GameConfig;
-import java.io.IOException;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 @Slf4j
 @UtilityClass
 public class ConfigLoader {
 
+  public static final String WORDS_CONFIG_FILE = "words.xml";
+
   public static GameConfig getGameconfig() {
     XmlMapper xmlMapper = new XmlMapper();
     try {
-      return xmlMapper.readValue(ResourceUtils.getFile("classpath:words.xml"), GameConfig.class);
+      Reader in = new InputStreamReader(new ClassPathResource(WORDS_CONFIG_FILE).getInputStream());
+      return xmlMapper.readValue(in, GameConfig.class);
     } catch (IOException e) {
       log.error("problem loading game configuration", e);
       throw new GameConfigLoadingException();
